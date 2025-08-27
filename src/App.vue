47cf6da07ab5816cf1272ee512417e7f8f20878b
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-08-27 13:32:57 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useRoute } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <Navbar v-if="!route.meta.noNavbarFooter" />
+    <router-view />
+    <Footer v-if="!route.meta.noNavbarFooter" />
+  </div>
+</template>
